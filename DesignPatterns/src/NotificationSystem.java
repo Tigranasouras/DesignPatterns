64e23b36be431dbd88ff55c5d9dd6effff9ae47a
Daron <daronbaltazar@gmail.com>
2024-11-26 05:12:22 -0600

    Notification 1userStory
    1. As a user, I want to receive basic notifications, so that I am informed about system updates.

--- DesignPatterns/src/NotificationSystem.java
+++ DesignPatterns/src/NotificationSystem.java
@@ -1,3 +1,9 @@
 public class NotificationSystem {
+    public static void main(String[] args) {
+        Notification smsNotification = new SMSNotification();
+
+        // Send a basic notification
+        smsNotification.send("System update: Maintenance scheduled for midnight.");
+    }
 
 }
