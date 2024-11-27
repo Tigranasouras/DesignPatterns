40b0128c2ae27ac79c28eb1240ae446aaf4c2ad3
Daron <daronbaltazar@gmail.com>
2024-11-26 22:01:48 -0600

    LoggerUser5
    5. As a system administrator, I want the logger to write logs to a file, so that I can retain logs for
    post-mortem analysis and audits.
    
    - I accidentally commited my last one where I did user stories 2-4 & 9-10 (I forgot to mention them in my commit log)

--- DesignPatterns/src/Logger.java
+++ DesignPatterns/src/Logger.java
@@ -1,10 +1,14 @@
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.FileWriter;
+
 
 public class Logger {
     private static Logger instance;
     private final List<String> logHistory = new ArrayList<>();
+
     private Logger() {}
 
     public static Logger getInstance() {
@@ -14,11 +18,22 @@ public class Logger {
         return instance;
     }
 
+    private void writeToFile(String logEntry){
+        String logFilePath = "Event.log"; // turned this into a local variable
+        try (FileWriter writer = new FileWriter(logFilePath, true)){
+            writer.write(logEntry + System.lineSeparator());
+        } catch (IOException e) {
+            System.err.println("Failed  to write log to file: " + e.getMessage()); //found this solution while re-learning how to do the writer.
+        }
+    }
+
     //logs message
     public void log(String condition, String message) {
         String logEntry = LocalDateTime.now() + " [" + condition + "] " + message;
         logHistory.add(logEntry);
         System.out.println(logEntry);
+        writeToFile(logEntry);
+
     }
 
     public List<String> getLogHistory() {
